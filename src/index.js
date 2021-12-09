80dbb9aebc696cd492bb2ecd130e89242e20dc42
Aleksei211 <85795990+Aleksei211@users.noreply.github.com>
2021-12-09 08:53:54 +0200

    added SimpleLightbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
+
 import './sass/main.scss';
+import galleryTpl from './templates/gallery.hbs';
+import NewsApiService from './js/NewsApiService';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn:document.querySelector('[data-action="load-more"]')
+}
+
+const newsApiService = new NewsApiService();
+
+//const API_KEY = '24706231-43c46d3eecbbb07f6810d4a25';
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+
+
+function onSearch(evt) {
+    evt.preventDefault();
+
+    clearHitsContainer();
+    newsApiService.searchQuery = evt.currentTarget.elements.searchQuery.value;
+
+    if (newsApiService.searchQuery === '') {
+        return Notify.failure('Sorry, enter your query')
+    }
+    newsApiService.resetPage();
+    newsApiService.fetchHits().then(appendHitsMarkup);
+    
+}
+
+function onLoadMore() {
+    newsApiService.fetchHits().then(appendHitsMarkup);
+}
+
+
+function appendHitsMarkup(hits) {
+    
+    refs.gallery.insertAdjacentHTML('beforeend', galleryTpl(hits));
+    lightbox.refresh()
+}
+function clearHitsContainer() {
+    refs.gallery.innerHTML = '';
+}
+
+ const lightbox = new SimpleLightbox('.gallery a', {
+   disableRightClick: true,
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt', 
+});
---
+++ src/js/NewsApiService.js
@@ -0,0 +1,36 @@
+const BASE_URL = 'pixabay.com/api';
+const API_KEY = '24706231-43c46d3eecbbb07f6810d4a25';
+
+
+export default class NewApiService{
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    fetchHits() {
+        
+        const url = `https://${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}
+    &image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=10`;
+
+ return fetch(url)
+    .then(response => response.json())
+    .then(({hits}) => {
+        this.incrementPage();
+        
+        return hits;
+    })
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
